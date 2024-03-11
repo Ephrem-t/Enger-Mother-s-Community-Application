8d486697f60133135ba6d3a8c5a61b3330199853
Bemnet <bemnettilahun700@gmail.com>
2024-03-11 15:55:20 +0300

    UserActivity is created

---
+++ app/src/main/java/com/example/engerapplication/activities/UserActivity.java
@@ -0,0 +1,16 @@
+package com.example.engerapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.engerapplication.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
